57cb4912d391905d128e4c978f1a48fd700bc327
Tedros Habtu <amhopefule07@gmail.com>
2024-12-25 15:51:35 +0300

    Changed this file to check vercel working for the last time

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cors());
 
 app.get('/',(request,response)=>{
     //console.log(request);
-    return response.status(234).send('Welcome to MERN stack tutorial full Stack Web Development');
+    return response.status(234).send('Welcome to MERN stack tutorial full Stack Web Development (React,Nodejs and MongoDB)');
 });
 
 app.use(express.urlencoded({extended:true}))  //Add this two lines of code to handle the image upload
